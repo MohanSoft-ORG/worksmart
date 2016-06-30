257ee847ed4d247f6c0f81402cbfd0bf7d8aea9f
Adrian Guzman <edgar.guzman21@gmail.com>
2016-06-30 13:21:57 -0500

    Se hacen cambios en gitignore y se agregan archivos al repositorio

---
+++ datos/constantes.php
@@ -0,0 +1,6 @@
+<?php
+define('DB_HOST','localhost');
+define('DB_NOMBRE_DATABASE','mohansof_worksmartdb');
+define('DB_USUARIO','mohansof_wsdb');
+define('DB_PUERTO','3307');
+define('DB_CLAVE','abc123.');
